209c7112c15ee35575e1c29374f93191679e2b5a
S. Dean Rider Jr <94093691+DeanRider@users.noreply.github.com>
2023-01-22 20:35:53 -0500

    Add files via upload

---
+++ Sam2RebuildSam.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# scan directory structure for sam results and store as a list
+find . -name "*.sam" -type file -exec echo '{}' >> SamRebuildList.txt \;
+
+#loop through list in the file and rebuild sam data for circos in later processes
+while read parseref; do
+
+echo $parseref
+# generated unique counted list with some errors that must be removed manually counts are last column
+
+
+
+
+
+#                                 *** ATTENTION ATTENTION ATTENTION ***
+
+# UPDATE AS NEEDED FOR ANY NEW ECTOPIC SITE NAMES
+
+grep -iE "@SQ|@PG|A35|B38|C12|N3|Dean_iPCR|eltaY6|a3ttr|CAG102" $parseref >> $parseref.Rebuild.sam || true
+
+
+
+
+
+
+echo -e "\033[1;34m -. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .\033[0m";
+echo -e "\033[1;30m ||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|\033[0m";
+echo -e "\033[1;30m |/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\||\033[0m";
+echo -e "\033[1;31m ~   \`-~ \`-\`   \`-~ \`-\`   \`-~ \`-~   \`-~ \`-\`   \`-~ \`-\`   \`-~ \`-\033[0m";
+
+
+done < "SamRebuildList.txt"
+
+
+echo
+echo
+echo
+echo -e "\033[1;31m ▄▄▄▄▄▄▄▄     ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░░░░░░░░▌   ▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m▐░█▀▀▀▀▀█░▌  ▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m▐░▌      ▐░▌ ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌          \033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m▐░▌      ▐░▌ ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌          \033[0m";
+echo -e "\033[1;31m▐░█▄▄▄▄▄█░▌  ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░░░░░░░░▌   ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m ▀▀▀▀▀▀▀▀     ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m                                                    \033[0m";
+
+
+
+exit 0
+
+
+
+
---
+++ Sam2dedupFasta.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#######################################################################
+# 
+# This script will scan a directory tree to create a list of input sam files 
+# It will then use each file in the list it created
+# and evaluate field 2 to separate primary mappings.
+# Each primary mapping represents a match  
+# awk will reformat this to 'seqname tab sequence' in a temp file 
+# The temporary file will be sorted on column 2 and deduplicated and piped to
+# awk to reformat it as fasta. The temp files will be removed.
+# 
+#######################################################################
+
+# scan directory structure for sam results and store as a list
+find . -name "*.sam" -type file -exec echo '{}' >> SamDedupList.txt \;
+mkdir dedups
+#loop through list in the file and parse data for circos
+while read parseref; do
+
+echo $parseref
+# generated unique counted list with some errors that must be removed manually counts are last column
+awk -F "\t" '{if($2=="0" || $2=="16") print $1,$10}' $parseref > $parseref.tmp || true
+cat $parseref.tmp | awk 'NR==1 || !a[$2]++' | awk '{print ">"$1"\n"$2}' > $parseref.dedup.fa
+
+echo -e "\033[1;34m -. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .\033[0m";
+echo -e "\033[1;30m ||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|\033[0m";
+echo -e "\033[1;30m |/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\|||/ \|||\||\033[0m";
+echo -e "\033[1;31m ~   \`-~ \`-\`   \`-~ \`-\`   \`-~ \`-~   \`-~ \`-\`   \`-~ \`-\`   \`-~ \`-\033[0m";
+rm $parseref.tmp
+cp $parseref.dedup.fa /dedups
+done < "SamDedupList.txt"
+rm SamDedupList.txt
+
+echo
+echo
+echo
+echo -e "\033[1;31m ▄▄▄▄▄▄▄▄     ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░░░░░░░░▌   ▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m▐░█▀▀▀▀▀█░▌  ▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m▐░▌      ▐░▌ ▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌          \033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌ ▐░▌   ▐░▌▐░█▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m▐░▌       ▐░▌▐░▌       ▐░▌▐░▌   ▐░▌ ▐░▌▐░█▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m▐░▌      ▐░▌ ▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌          \033[0m";
+echo -e "\033[1;31m▐░█▄▄▄▄▄█░▌  ▐░█▄▄▄▄▄▄▄█░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄▄▄ \033[0m";
+echo -e "\033[1;31m▐░░░░░░░░▌   ▐░░░░░░░░░░░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌\033[0m";
+echo -e "\033[1;31m ▀▀▀▀▀▀▀▀     ▀▀▀▀▀▀▀▀▀▀▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀ \033[0m";
+echo -e "\033[1;31m                                                    \033[0m";
+
+
+
+exit 0
+
+
+
+
